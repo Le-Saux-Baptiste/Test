4aa0d4ff2a16acab43baba53a9a0b57f48056b71
Le-Saux-Baptiste <lesauxbaptiste@gmail.com>
2016-05-31 12:34:44 +0200

    Update main.java

--- src/main/java/myGreatProject/main.java
+++ src/main/java/myGreatProject/main.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.base.Strings;
 
+
 public class main {
 	public static void main(String[] args) {
 		System.out.println(Strings.isNullOrEmpty("pas vide"));
