7753691ddbc165016ede30b4d4ef1bd6cec3acc2
Baptiste <lesauxbaptiste@gmail.com>
2016-05-31 11:33:56 +0200

    gitignore + main

---
+++ src/main/java/myGreatProject/main.java
@@ -0,0 +1,12 @@
+package myGreatProject;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Strings;
+
+public class main {
+	public static void main(String[] args) {
+		System.out.println(Strings.isNullOrEmpty("pas vide"));
+		System.out.println(StringUtils.isEmpty("truc"));	
+	}
+}
